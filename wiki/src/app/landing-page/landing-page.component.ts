2952e7ab1afdc6413fd41f34636f44700bd2344f
F0xedb <tom@odex.be>
2020-01-21 18:56:54 +0100

    Added landing page

--- wiki/src/app/landing-page/landing-page.component.ts
+++ wiki/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { GeneralService } from "../services/general.service";
 
 @Component({
   selector: "app-landing-page",
@@ -7,9 +8,24 @@ import { Router } from "@angular/router";
   styleUrls: ["./landing-page.component.scss"]
 })
 export class LandingPageComponent implements OnInit {
-  constructor(private router: Router) {
-    this.router.navigate(["page", "0"]);
+  logo = "https://tos.odex.be/images/logo.svg";
+  name = "WikiTS";
+  description = "The best cheatsheet ever";
+  constructor(private router: Router, private general: GeneralService) {
+    let interval = setInterval(() => {
+      if (this.general.data !== undefined) {
+        console.log(this.general.data);
+        this.logo = this.general.data.logo;
+        this.name = this.general.data.name;
+        this.description = this.general.data.description;
+        clearInterval(interval);
+      }
+    }, 200);
   }
 
   ngOnInit() {}
+
+  home() {
+    this.router.navigate(["page", "0"]);
+  }
 }
